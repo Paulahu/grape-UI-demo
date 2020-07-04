78c5cfa270794acac5c76e74f65457cb5524b542
Paulahu <paulahu1995@gmail.com>
2020-07-04 16:09:40 +0800

    添加icon和iconPosition属性

--- dist/App.f684dadd.js
+++ dist/App.f684dadd.js
@@ -12438,8 +12438,16 @@ exports.default = void 0;
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
 var _default = {
-  name: 'Button.vue'
+  name: 'Button.vue',
+  props: ['icon', 'iconPosition']
 };
 exports.default = _default;
         var $ea7f05 = exports.default || module.exports;
@@ -12451,10 +12459,26 @@ exports.default = _default;
         /* template */
         Object.assign($ea7f05, (function () {
           var render = function() {
+  var _obj
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("button", { staticClass: "g-button" }, [_vm._v("按钮")])
+  return _c(
+    "button",
+    {
+      staticClass: "g-button",
+      class: ((_obj = {}), (_obj["icon-" + _vm.iconPosition] = true), _obj)
+    },
+    [
+      _vm.icon
+        ? _c("svg", { staticClass: "icon" }, [
+            _c("use", { attrs: { "xlink:href": "#i-" + _vm.icon } })
+          ])
+        : _vm._e(),
+      _vm._v(" "),
+      _c("div", { staticClass: "content" }, [_vm._t("default")], 2)
+    ]
+  )
 }
 var staticRenderFns = []
 render._withStripped = true
@@ -12534,7 +12558,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60311" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65522" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
