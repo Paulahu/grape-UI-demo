5dd9ed386033149389c5b1da5f63d858bd1ac399
Paulahu <paulahu1995@gmail.com>
2020-07-05 22:41:28 +0800

    rebuild

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Vue from  'vue'
+// import Button from './Button'
+// import Icon from './Icon'
+
+// Vue.component('g-button',Button)
+// Vue.component('g-icon', Icon)
+
+new Vue({
+  el: '#app',
+  data: {
+    message: '111'
+  }
+})
